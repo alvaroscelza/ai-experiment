0a89179adecbf204e25a3a3b28900414015aaac5
Alvaro Scelza <alvaroscelza@gmail.com>
2024-06-23 14:17:19 -0300

    reducing variability in results

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ os.environ['PYTHONHASHSEED'] = str(RANDOM_STATE)
 np.random.seed(RANDOM_STATE)
 tf.random.set_seed(RANDOM_STATE)
 
-# Configure TensorFlow to use a single thread
-tf.config.threading.set_intra_op_parallelism_threads(1)
+# Configure TensorFlow to use a single thread and set deterministic operations
+os.environ['TF_DETERMINISTIC_OPS'] = '1'
+os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
 tf.config.threading.set_inter_op_parallelism_threads(1)
+tf.config.threading.set_intra_op_parallelism_threads(1)
 
 # Load and preprocess data
 data_loader = DataLoader(file_path='reviews.csv')
